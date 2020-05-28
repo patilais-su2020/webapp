f24ad36a1d6c3e86eaddd8406adfea40e96c4c32
Aishwarya Patil <patil.ais@northeastern.edu>
2020-05-28 15:30:00 -0400

    Changes unit test

--- backend/tests/apitest.js
+++ backend/tests/apitest.js
@@ -4,58 +4,20 @@ request = require('supertest')
 should = require('should')
 var expect = chai.expect;
 
-describe('MySQL connection check: ', () => {
-  it('Should check for database connection', (done) => {
+describe('POST /user/register', function () {
+  it('responds with json error', function (done) {
     request(app)
       .post('/user/register')
-      .end((err, res) => {
-        console.log("res")
-        console.log(res)
-        console.log("err")
-        console.log(err)
-        if (err) {
-          console.error("ERROR:", err);
-          err.should.have("errno")
-        } else {
-          console.log("Connected to database successfully")
-          describe('/POST /user/register', () => {
-            it('responds with json with error', (done) => {
-              request(app)
-                .post('/user/register')
-                .send({ firstName: "Aishwarya", lastName: null, email: 'user@email.com', password: 'yourpassword' })
-                .set('Accept', 'application/json')
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .expect(500)
-                .end(function (err, res) {
-                  console.log(res.text)
-                  if (res.text.should.equal('{"error":"Column \'lastName\' cannot be null"}')) {
-                    expect(res.statusCode).to.equal(500);
-                    done();
-                  }
-                })
-            });
-          });
+      .send({ firstName: "Aishwarya", lastName: null, email: null, password: 'yourpassword' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(400)
+      .end(function (err, res) {
+        console.log(res.text)
+        if (res.text.should.equal('{"status":400,"message":"Email can not be empty!"}')) {
+          expect(res.statusCode).to.equal(400);
+          done();
         }
-        done();
-      });
+      })
   });
 });
-
-
-// describe('POST /user/register', function () {
-//   it('responds with json', function (done) {
-//     request(app)
-//       .post('/user/register')
-//       .send({ firstName: "Aishwarya", lastName: null, email: 'user@email.com', password: 'yourpassword' })
-//       .set('Accept', 'application/json')
-//       .expect('Content-Type', 'application/json; charset=utf-8')
-//       .expect(500)
-//       .end(function (err, res) {
-//         console.log(res.text)
-//         if (res.text.should.equal('{"error":"Column \'lastName\' cannot be null"}')) {
-//           expect(res.statusCode).to.equal(500);
-//           done();
-//         }
-//       })
-//   });
-// });
