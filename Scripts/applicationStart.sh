70e743b5e17d4694edd0470b4b0f1fe6b34bba88
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 19:23:20 -0400

    Changes to add environment variables

--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
+sudo pm2 kill
 cd /home/ubuntu/backend/
-sudo -E --preserve-env pm2 --name backend start npm -- start --update-env
+sudo -E --preserve-env pm2 --name backend restart npm -- start --update-env
 cd /home/ubuntu/frontend/
-sudo -E --preserve-env pm2 --name frontend start npm -- start --update-env
+sudo -E --preserve-env pm2 --name frontend restart npm -- start --update-env
