a3b4169ec75922d1702e297d9f23f0aec409c5a3
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 17:38:54 -0400

    Changes to add environment variables

--- Scripts/afterInstall.sh
+++ Scripts/afterInstall.sh
@@ -3,7 +3,7 @@
 sudo npm install pm2 -g
 cd /home/ubuntu/backend/
 sudo npm install
-pm2 --name webapp start npm -- start
+sudo -E pm2 --name webapp start npm -- start
 cd /home/ubuntu/frontend/
 sudo npm install
-pm2 --name webapp start npm -- start
+sudo -E pm2 --name webapp start npm -- start
