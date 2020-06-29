cbecc6bf99626d593055b90c410ba817ec29b550
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 23:44:11 -0400

    Changes in sh file

--- Scripts/afterInstall.sh
+++ Scripts/afterInstall.sh
@@ -4,7 +4,9 @@ sudo npm install pm2 -g
 cd
 cd /home/ubuntu/backend/
 sudo npm install
-sudo -E --preserve-env pm2 --name backend start npm -- start
-cd /home/ubuntu/frontend/
-sudo npm install
-sudo -E --preserve-env pm2 --name backend start npm -- start
+sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
+source ../.bashrc && pm2 startOrRestart --name backend npm -- start
+# sudo -E --preserve-env pm2 --name backend start npm -- start
+# cd /home/ubuntu/frontend/
+# sudo npm install
+# sudo -E --preserve-env pm2 --name backend start npm -- start
--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-cd /home/ubuntu/backend/
-sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
-source ../.bashrc && pm2 startOrRestart --name backend npm -- start
+echo "----"
