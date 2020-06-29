e1878c5d1ac11f072944a337a4c0827acd0a942b
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 20:34:40 -0400

    changes

--- Scripts/afterInstall.sh
+++ Scripts/afterInstall.sh
@@ -4,5 +4,7 @@ sudo npm install pm2 -g
 cd
 cd /home/ubuntu/backend/
 sudo npm install
+sudo -E --preserve-env pm2 --name backend restart npm -- start --update-env
 cd /home/ubuntu/frontend/
 sudo npm install
+sudo -E --preserve-env pm2 --name backend restart npm -- start --update-env
