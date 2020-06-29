a6483c1266272060b9ac996961cb5a9e2bae9fa0
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 20:22:49 -0400

    changes

--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -3,7 +3,9 @@
 sudo pm2 kill
 cd /home/ubuntu/backend/
 sudo -E --preserve-env pm2 --name backend start npm -- start
+cd /home/ubuntu/backend/
 sudo -E --preserve-env pm2 --name backend restart npm -- start --update-env
 cd /home/ubuntu/frontend/
 sudo -E --preserve-env pm2 --name frontend start npm -- start
+cd /home/ubuntu/frontend/
 sudo -E --preserve-env pm2 --name frontend restart npm -- start --update-env
