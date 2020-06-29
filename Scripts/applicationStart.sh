f143ce006d12b0506b7272f1551e416ce174e5bf
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 23:21:48 -0400

    Changes in sh file

--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -2,4 +2,4 @@
 
 cd /home/ubuntu/backend/
 sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
-source .bashrc && pm2 startOrRestart --name backend npm --start
+source ../.bashrc && pm2 startOrRestart --name backend npm --start
