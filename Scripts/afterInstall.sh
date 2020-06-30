019902610eab225ef1f22872cc62e1224d0d0028
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-30 19:54:46 -0400

    Changes in pm2

--- Scripts/afterInstall.sh
+++ Scripts/afterInstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
-
+sudo npm install forever -g
 sudo npm install pm2 -g
 cd
 cd /home/ubuntu/backend/
 sudo npm install
 sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
 source ../.bashrc 
-db_hostname=$db_hostname db_name=$db_name db_username=$db_username db_password=$db_password pm2 start --name backend npm -- start
+db_hostname=$db_hostname db_name=$db_name db_username=$db_username db_password=$db_password forever start app.js
 # sudo -E --preserve-env pm2 --name backend start npm -- start
 # cd /home/ubuntu/frontend/
 # sudo npm install
