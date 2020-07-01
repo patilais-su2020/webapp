5286e9043038b89aae26f8c0b822ba27015d8fcf
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-01 00:44:27 -0400

    changes in afterinstall.sh

--- Scripts/afterInstall.sh
+++ Scripts/afterInstall.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/backend/
 sudo npm install
 sudo chown ubuntu:ubuntu /home/ubuntu/.pm2/rpc.sock /home/ubuntu/.pm2/pub.sock
 source ../.bashrc 
-db_hostname=$db_hostname db_name=$db_name db_username=$db_username db_password=$db_password forever start app.js
+db_hostname=$db_hostname db_name=$db_name db_username=$db_username db_password=$db_password forever start --minUptime 2000 -l forever.log -o out.log -e err.log app.js
 # sudo -E --preserve-env pm2 --name backend start npm -- start
 # cd /home/ubuntu/frontend/
 # sudo npm install
