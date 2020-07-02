897df68c5825f0020361ebb9e79c7d6638542d3b
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-01 20:52:22 -0400

    changes in afterinstall.sh

--- Scripts/applicationStop.sh
+++ Scripts/applicationStop.sh
@@ -3,5 +3,6 @@
 sudo rm -rf /home/ubuntu/*
 sudo rm -rd sudo rm -rf /opt/codedeploy-agent/deployment-root/*
 sudo service codedeploy-agent restart
+cd /home/ubuntu
 sudo pm2 kill
 sudo pm2 delete all
