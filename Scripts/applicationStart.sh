541dbc8eb726f270ff03d83bc7aaff130f7ce5b7
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-07 15:57:45 -0400

    Added config folder to zip for codedeploy

--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -2,9 +2,6 @@
 
 source /etc/environment
 
-ls
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloud-config/cloud-watch-config.json -s
 sudo systemctl start amazon-cloudwatch-agent.service
 
 cd /home/ubuntu/backend/
