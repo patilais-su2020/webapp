1585026470dcf24e0b1ca54f7d547af1a87bc387
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-07 02:14:19 -0400

    changes in after install

--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -2,6 +2,11 @@
 
 source /etc/environment
 
+ls
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloud-config/cloud-watch-config.json -s
+sudo systemctl start amazon-cloudwatch-agent.service
+
 cd /home/ubuntu/backend/
 sudo pm2 start npm -- start
 
