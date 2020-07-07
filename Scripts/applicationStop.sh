b8244115e07ba816b786f9110de852402931e4e9
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-06 23:17:17 -0400

    Fixed multiple deployment issue and added cloud watch config

--- Scripts/applicationStop.sh
+++ Scripts/applicationStop.sh
@@ -4,6 +4,9 @@ sudo rm -rf /home/ubuntu/*
 # sudo rm -rf /opt/codedeploy-agent/deployment-root/*
 # sudo service codedeploy-agent restart
 sudo service code-deploy-agent status
+sudo systemctl restart amazon-cloudwatch-agent.service
+
+
 cd /home/ubuntu
 sudo pm2 kill
 sudo pm2 delete all
