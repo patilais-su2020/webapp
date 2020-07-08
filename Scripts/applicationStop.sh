d98c8139920f8e48cd07cf7d76ff1fa14bc81159
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-07 23:46:04 -0400

    Changes in applicationStop

--- Scripts/applicationStop.sh
+++ Scripts/applicationStop.sh
@@ -3,7 +3,7 @@
 sudo pm2 kill || true
 sudo pm2 delete all || true
 sudo rm -rf /home/ubuntu/*
-sudo rm -rf /opt/codedeploy-agent/deployment-root/*
-sudo service codedeploy-agent restart
-sudo service codedeploy-agent status
-sudo systemctl stop amazon-cloudwatch-agent.service
+# sudo rm -rf /opt/codedeploy-agent/deployment-root/*
+# sudo service codedeploy-agent restart
+# sudo service codedeploy-agent status
+# sudo systemctl stop amazon-cloudwatch-agent.service
