e58d9d16052d7fbe4194223e19929bf3b1c29e9b
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-09 17:06:58 -0400

    Changes on restart

--- Scripts/applicationStart.sh
+++ Scripts/applicationStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 source /etc/environment
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u root --hp /home/ubuntu
 
 sudo systemctl start amazon-cloudwatch-agent.service
 
