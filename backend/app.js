fd28193fbbf53653cc0ec1f390b33dc96a12fd19
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-09 15:37:30 -0400

    Changes in cloud watch config and removed statsdconfig

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,44 @@ app.use((req,res,next) => {
     next();
 });
 
+var StatsD = require('statsd-client'),
+booklisting_counter = new StatsD({port: 8125, prefix: 'booklisting_counter'});
+api_execution_time = new StatsD({port: 8125, prefix: 'api_execution_time'})
+query_execution_time = new StatsD({port: 8125, prefix: 'query_execution_time'})
+s3_execution_time = new StatsD({port: 8125, prefix: 's3_execution_time'})
+
+app.use(function (req, res, next) {
+   
+    if(req.url.includes("/allbooks")){
+        booklisting_counter.counter('fetch_all_book_listing_counter',1)
+    }
+    if(req.url.includes("/booksposted")){
+        booklisting_counter.counter('books_posted_listing_counter',1)
+    }
+
+    var start = process.hrtime();
+    res.once('finish', function() {
+
+        console.log("Request for::",req.url )
+
+        var diff = process.hrtime(start);
+        var ms = diff[0] * 1e3 + diff[1] * 1e-6;
+        
+        console.log('The request processing time is %d ms.', ms);
+
+        var diff1 = process.hrtime(start);
+        var ms1 = diff1[0] * 1e3 + diff1[1] * 1e-6;
+
+        if(req.url.includes("/uploads3image") || req.url.includes("/deletefroms3") || req.url.includes("/deleteallimagesfroms3") ){
+            s3_execution_time.timing(req.url,ms)
+        }else{
+            query_execution_time.timing(req.url,ms)
+            api_execution_time.timing(req.url,ms1)
+        }
+    });
+    next();
+});
+
 app.use('/user', userRoutes);
 app.use('/books', booksRoutes);
 app.use('/cart', cartRoutes);
