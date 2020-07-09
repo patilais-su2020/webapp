04c1b070800038ec446d78ac3acc20837990a19e
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-09 14:38:21 -0400

    Changes in cloud watch config and statsd config

--- backend/statsdconfig.js
+++ backend/statsdconfig.js
@@ -5,5 +5,6 @@ export default {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
         region: process.env.AWS_REGION,
+        namespace: "CSYE6225_CW_Agent"
     }
 }
