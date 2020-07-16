80fdfa5276896c862058649b98c986ca8106d327
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-16 17:54:27 -0400

    Changes in SetupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/',
     createProxyMiddleware({
       target: 'http://prod.neucloudwebapp.me:5000',
       changeOrigin: true,
